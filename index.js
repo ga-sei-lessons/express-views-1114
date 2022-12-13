51eb4bd05083c91bd5efcd697d3f305127b08b36
weston-bailey <wdbailey87@gmail.com>
2022-12-13 12:30:13 -0800

    update

--- index.js
+++ index.js
@@ -4,11 +4,25 @@ const express = require('express')
 // app config
 const app = express()
 const PORT = 8080
+// tell express to use ejs for rendering
+app.set('view engine', 'ejs')
 
 // routes
 // GET / -- send a view of  the home page
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/views/index.html')
+    // // when sending a static file, express needs an absolute path, hence the __dirname
+    // res.sendFile(__dirname + '/views/index.html')
+    // res.render prints out html from a template
+    // ejs assumes there is a folder named 'views'
+    // and takes a relative file path in the views folder
+    // res.render('file name'. { data object })
+    // pull data from your database
+    //  make API request
+    res.render('index.ejs', {
+        name: 'Sterling Archer',
+        age: 10,
+        faveFood: ['sushi', 'pizza', 'ice cream', 'pb&j']
+    })
 })
 
 // GET /about -- send the about apge
